37315f03e4fe864c71337c6b6b863c872f467ac6
verissimon <terceiroverissimo@gmail.com>
2025-08-11 20:58:20 -0300

    Implement friend addition and random selection functionality

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    const nomeAmigo = document.querySelector(".input-name").value
+    document.querySelector(".input-name").value = ""
+    if (amigos.includes(nomeAmigo)) {
+        alert(nomeAmigo + " ja foi adicionado");
+        return
+    }
+
+    if (!nomeAmigo) {
+        return
+    }
+
+    amigos = [...amigos, nomeAmigo]
+    const listaAmigos = document.querySelector(".name-list")
+    const elementoAmigo = document.createElement("p")
+    elementoAmigo.textContent = `${amigos.length}. ${nomeAmigo}`
+    listaAmigos.appendChild(elementoAmigo)
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const resultado = document.createElement("p")
+
+function sortearAmigo() {
+    if (!amigos.length) {
+        alert("Nenhum amigo para sortear. Adicione alguém primeiro")
+    }
+
+    const listaResultado = document.querySelector(".result-list")
+    const amigoSorteado = amigos[getRandomInt(amigos.length)]
+    resultado.textContent = "Amigo sorteado: " + amigoSorteado
+    listaResultado.appendChild(resultado)
+}
